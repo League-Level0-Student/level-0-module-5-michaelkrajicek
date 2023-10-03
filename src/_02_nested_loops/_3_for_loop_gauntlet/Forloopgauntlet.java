4b9c73b4d90a55417f1caa1b42776b6b316b7045
student <student@192.168.1.17>
2023-10-03 13:59:26 -0700

    hi

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/Forloopgauntlet.java
@@ -0,0 +1,75 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+public class Forloopgauntlet {
+	public static void main(String[] args) {
+		
+		//for (int i = 0; i<101; i++) {
+			//System.out.println(i+" ");
+		//}
+		
+		//for (int i = 100; i>0; i--) {
+			//System.out.println(i);
+		//}
+		
+		//for (int i = 0; i<101; i+=2) {
+				//System.out.println(i);
+		//}
+		
+		//for (int i = 1; i<100; i+=2) {
+			//System.out.println(i);
+		//}
+		
+		//for (int j = 1; j<501; j++) {
+			//if (j % 2 == 0) {
+				//System.out.println(j+" is even");
+			//} else {
+				//System.out.println(j+" is odd");
+			//}
+			//}
+			
+			//for (int i = 0; i<778; i+=7) {
+					//System.out.println(i);
+			//}
+			
+			//for (int i = 0, a = 2006; i <17 && a <2023; i++, a++) {
+				
+				//System.out.println("In December of "+a+" I was "+i+" years old.");
+			//}
+			
+		//for (int i = 0; i<3; i++){
+			//for (int a = 0; a<3; a++) {
+				//System.out.println(i+ " " + a);
+			//}
+			
+		//}
+		
+		//for (int i=0; i<3; i++) {
+			
+			
+			//for (int j=1; j<4; j++) {
+				
+			//	System.out.print(j+(i*3));
+			//} 
+		//	System.out.println();
+		//}
+		
+		//for (int i=0; i<10; i++) {	
+			
+		//	for (int j=1; j<11; j++) {
+				
+		//		System.out.print((j + i * 10)+" ");
+		//	} 
+		//	System.out.println();
+		//}
+		
+		for (int j=1; j<8; j++) {
+			for (int k = 0; k<j; k++) {
+				
+			System.out.print("* ");
+		} System.out.println();
+			}
+		
+		}
+		
+	}
+
